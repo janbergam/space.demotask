4ab75e5a918a906ef7c62271351639174e2a6010
Jan Bergam <jan.bergam@sk.ibm.com>
2019-09-11 21:00:35 +0200

    firts commit for template

---
+++ src/main/java/space/demotask/DemotaskApplication.java
@@ -0,0 +1,13 @@
+package space.demotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotaskApplication.class, args);
+	}
+
+}
